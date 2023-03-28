c9fb078f970e1f8661665ab158e41c1d3cded76a
Jasmine Grozinger <jasmine.grozinger@gmail.com>
2023-03-28 12:13:18 +0200

    Update to PokemonCard file

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,4 +1,17 @@
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
 function PokemonCard(){
+    // const pokemon = pokemonList[0]
+    // return pokemon === pokemonList.imgSrc ? pokemonList.imgSrc : <p>???</p>
     return (
         <figure>
             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur">
@@ -9,3 +22,4 @@ function PokemonCard(){
 }
 
 export default PokemonCard;
+

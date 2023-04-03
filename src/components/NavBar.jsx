7aae2d83dc3b79bd6a57177fcf83e60f534f6b78
Jasmine Grozinger <jasmine.grozinger@gmail.com>
2023-04-03 14:33:04 +0200

    React 08 Répéter un composant avec map

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,11 @@
-import { useState } from "react";
 function NavBar({ count, pokemonIndex, pokemonList }) {
-  const handleClickNext = () => {
-    pokemonIndex(count + 1);
-  };
-  const handleClickPrevious = () => {
-    pokemonIndex(count - 1);
-  };
-  const isNext = count < pokemonList.length - 1;
-  const isPrevious = count > 0;
-
   return (
     <nav>
-      {isPrevious ? (
-        <button onClick={handleClickPrevious}>Précédent</button>
-      ) : null}
-      {isNext ? <button onClick={handleClickNext}>Suivant</button> : null}
+      {pokemonList.map((el, index) => (
+        <button key={index} onClick={() => pokemonIndex((count = index))}>
+          {el.name}
+        </button>
+      ))}
     </nav>
   );
 }
